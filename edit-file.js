5f43ce5c508ac8489d8a111c003621f7d1d4ac96
BECCA LEE <REBECCA.KIRSTIN.LEE@GMAIL.COM>
2018-12-19 19:43:17 -0800

    Finishes task 1, fixes broken code

--- edit-file.js
+++ edit-file.js
@@ -4,19 +4,23 @@ let fs = require('fs');
 
 let file = `./files/test.txt`;
 
+let randVal = Math.random();
+
+
 fs.readFile(file, (err, data) => {
   if (err) throw err;
   console.log('before', data.toString());
+  
+  fs.writeFile(file, randVal, (err) => {
+    if (err) throw err;
+    console.log('Random number saved to test.txt file!');
+
+    fs.readFile(file, (err, data) => {
+      if (err) throw err;
+      console.log('after', data.toString());
+    });
+  });
 });
 
-let randVal = Math.random();
 
-fs.writeFile(file, randVal, (err) => {
-  if (err) throw err;
-  console.log('Random number saved to test.txt file!');
-});
 
-fs.readFile(file, (err, data) => {
-  if (err) throw err;
-  console.log('after', data.toString());
-});
